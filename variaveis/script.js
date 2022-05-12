4d6a81db4325277a6a8993acec12ac08c3540908
Vilson <code.devjunior@outlook.com.br>
2022-05-12 20:20:55 -0300

    Iniciando curso de JavascriptES6

---
+++ variaveis/script.js
@@ -0,0 +1,21 @@
+/**
+ * Responsvel por guardar dados na memoria.
+ * Inicia com a palavra var, let ou const.
+ */
+
+var nome = "Vilson";
+var idade = 29;
+const possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+/**
+ * Evitam repetição
+ * DRY (Don't repeat yourself)
+ */
+
+var preco = 20;
+var totalComprado = 5;
+var precoTotal = preco * totalComprado;
+
+console.log(precoTotal);
